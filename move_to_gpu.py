a1c5774f5868aa5c2f203883920f88d6edcd7d10
kabellabalazs <balazs.kabella@gmail.com>
2025-02-04 09:06:55 +0100

    move scripts to scripts folder

--- move_to_gpu.py
+++
@@ -1,3 +0,0 @@
-import torch
-def move2gpu(x):
-    return torch.tensor(x,dtype=torch.complex128).to(device='cuda')
--- options.py
+++
@@ -1,9 +0,0 @@
-
-
-prec=1e-9
-max_bond=200
-options={}
-options['split_opts']={'method':'svd','cutoff':prec,'cutoff_mode':'rel','max_bond':max_bond}
-options['tebd_opts']={'tol':prec,'order':4,'progbar':False}
-options['comp_opts']={'method':'svd','cutoff':prec,'cutoff_mode':'rel'}
-
